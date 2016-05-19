943c96ee6794a37e34b5144f247d5f3adc2ec2b4
Prateek Kumar <prateek@prateekkumar.in>
2016-05-19 20:20:00 +0530

    Updated

--- public/js/script.js
+++ public/js/script.js
@@ -1,26 +1,31 @@
-$.fn.extend({
-    animateCss: function (animationName, callback) {
-        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-        $(this).addClass('animated ' + animationName).one(animationEnd, function() {
-            $(this).removeClass('animated ' + animationName);
-            if(callback) callback($(this));
-        });
-        return $(this);
-    }
-});
+// Background slideshow
 
-$(document).ready(function() {
-  $(".nav-open").click(function(){
-    $("nav").show().animateCss("fadeInRight");
-  });
-  $(".nav-close").click(function(){
-    $("nav").animateCss("fadeOutRight", function(el) {el.hide();});
-  });
-  $("nav a").click(function(e) {
-    e.preventDefault();
-    var el = $(this);
-    $('html, body').animate({
-        scrollTop: $(el.attr("href")).offset().top
-    }, 1000);
-  });
-});
+$(document).ready(function(){
+ 
+ var imgArr = new Array( // relative paths of images
+ 'img/img-1.jpg',
+ 'img/img-2.jpg',
+ 'img/img-3.jpg',
+ 'img/img-4.jpg'
+ );
+ 
+ var preloadArr = new Array();
+ var i;
+ 
+ /* preload images */
+ for(i=0; i < imgArr.length; i++){
+ preloadArr[i] = new Image();
+ preloadArr[i].src = imgArr[i];
+ }
+ 
+ var currImg = 1;
+ var intID = setInterval(changeImg, 3000);
+ 
+ /* image rotator */
+ function changeImg(){
+ $('.initial-div').animate({opacity: 0}, 1000, function(){
+ $(this).css('background','url(' + preloadArr[currImg++%preloadArr.length].src +') top center no-repeat');
+ }).animate({opacity: 1}, 1000);
+ }
+ 
+ });
